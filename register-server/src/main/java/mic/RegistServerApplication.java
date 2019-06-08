3b04c9906f978b4c95491b1de8c61ee0f1185ede
chenshuai <chenshuai@galasports.net>
2019-06-08 14:21:31 +0800

    init project

---
+++ register-server/src/main/java/mic/RegistServerApplication.java
@@ -0,0 +1,13 @@
+package mic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistServerApplication.class, args);
+	}
+
+}
