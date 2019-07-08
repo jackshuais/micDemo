1f03c95bc38e0e2300fc4073756b57fe4c2092de
chenshuai <chenshuai@galasports.net>
2019-07-08 19:06:12 +0800

    change some property

--- user-server/src/main/java/mic/UserServerApplication.java
+++ user-server/src/main/java/mic/UserServerApplication.java
@@ -2,8 +2,10 @@ package mic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServerApplication {
 
 	public static void main(String[] args) {
